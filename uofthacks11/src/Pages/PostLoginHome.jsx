650088a8bfe8311644db7266533194a115d29c80
ZNLake <91035765+ZNLake@users.noreply.github.com>
2024-01-28 02:17:18 -0500

    SPotify authorization request implemented

--- uofthacks11/src/Pages/PostLoginHome.jsx
+++ uofthacks11/src/Pages/PostLoginHome.jsx
@@ -6,8 +6,100 @@ import VerticalCarousel from '../components/VerticalCarousel.jsx';
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from 'react';
 
+const generateRandomString = (length) => {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+
+const codeVerifier  = generateRandomString(64);
+
+const sha256 = async (plain) => {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(plain)
+  return window.crypto.subtle.digest('SHA-256', data)
+}
+
+const base64encode = (input) => {
+  return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+const hashed = await sha256(codeVerifier)
+const codeChallenge = base64encode(hashed);
+
+const clientId = 'fa413c9476dc4216ba24e2150c1ff7f9';
+const redirectUri = 'http://localhost:15000';
+
+const scope = 'playlist-modify-public playlist-modify-private user-read-email user-read-private';
+const authUrl = new URL("https://accounts.spotify.com/authorize")
+
+// generated in the previous step
+window.localStorage.setItem('code_verifier', codeVerifier);
+
+const params =  {
+  response_type: 'code',
+  client_id: clientId,
+  scope,
+  code_challenge_method: 'S256',
+  code_challenge: codeChallenge,
+  redirect_uri: redirectUri,
+}
+
+authUrl.search = new URLSearchParams(params).toString();
+window.location.href = authUrl.toString();
+
+const urlParams = new URLSearchParams(window.location.search);
+let code = urlParams.get('code');
+let state = urlParams.get('state');
+
+const getToken = async code => {
+
+  // stored in the previous step
+  let codeVerifier = localStorage.getItem('code_verifier');
+
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      client_id: clientId,
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: redirectUri,
+      code_verifier: codeVerifier,
+    }),
+  }
+
+  const body = await fetch(url, payload);
+  const response =await body.json();
+
+  localStorage.setItem('access_token', response.access_token);
+
+    // Send state, code, and access token to a route
+  const routePayload = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      state, // Use state from URL parameters
+      code,
+      access_token: response.access_token,
+    }),
+  }
+
+  await fetch('localhost:15000/recieve_json', routePayload);
+}
+
 function PostLogin() {
   const { user, isAuthenticated, isLoading } = useAuth0();
+
+
+
 
   // Your fetch request or any other side effect
   useEffect(() => {
