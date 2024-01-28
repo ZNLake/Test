7140243bb6b20337a7697b23842b7a023659460d
katarina <kavucic@icloud.com>
2024-01-27 21:25:02 -0500

    fixed docker!

--- uofthacks11/server/app.py
+++ uofthacks11/server/app.py
@@ -125,6 +125,5 @@ def serve(path):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='172.17.0.2' ,port=5002)
-
-    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
